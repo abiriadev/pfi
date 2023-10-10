12cdf796c5e61eb576f1da1493cfad4e86d08996
Abiria <abiria.dev@gmail.com>
2023-10-10 16:56:39 +0900

    feat: implement basic behavior

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::io::BufRead;
+use std::io::{self, BufRead};
 
 pub enum FileIdentity {
 	Text,
@@ -6,4 +6,19 @@ pub enum FileIdentity {
 	Unknown,
 }
 
-pub fn identifier<T: BufRead>(data: T) -> FileIdentity { todo!() }
+static PORTABLE_NETWORK_GRAPHICS: [u8; 4] = [0x89, 0x50, 0x4E, 0x47];
+
+pub fn identifier<T: BufRead>(data: &mut T) -> io::Result<FileIdentity> {
+	let mut buf = [0u8; 512];
+	data.read_exact(&mut buf)?;
+
+	if buf[..4] == PORTABLE_NETWORK_GRAPHICS {
+		return Ok(FileIdentity::PortableNetworkGraphics);
+	};
+
+	if buf.iter().all(u8::is_ascii) {
+		return Ok(FileIdentity::Text);
+	};
+
+	Ok(FileIdentity::Unknown)
+}
